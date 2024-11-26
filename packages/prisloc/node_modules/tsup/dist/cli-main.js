34423799b13d6f4d50cd08fc36ef220d5d238c9c
Lucas Matheus <lucasmatheus2021@gmail.com>
2024-11-26 12:12:00 -0300

    fix: changed gitignore
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- packages/prisloc/node_modules/tsup/dist/cli-main.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});
-
-var _chunkHYBDOOZ5js = require('./chunk-HYBDOOZ5.js');
-require('./chunk-4FJ6UJ7E.js');
-require('./chunk-JIBD5LVI.js');
-
-
-exports.main = _chunkHYBDOOZ5js.main;
